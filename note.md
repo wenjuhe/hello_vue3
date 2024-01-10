11d93c65bc6f29447fcab820e6557872f558fa78
wangze <18802401646@163.com>
2024-01-10 20:06:35 +0800

    first commit

---
+++ note.md
@@ -0,0 +1,4 @@
+### 编写App组价
+Vite 项目中, index.html 是项目的入口文件, 在项目最外层.
+加载 index.html 后, Vite 解析 <script type="module" src="xxx"> 指向的 JavaScript
+Vue3 中是通过 createApp 函数创建一个应用实例
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+//.vue文件中能写什么?  三个标签 template script style
+
+
+<template>
+    <div class="app">
+        <h1>你好</h1>
+    </div>
+</template>
+
+
+<script lang="ts">
+  export default {
+    name: 'App' //组件名
+  }
+</script>
+
+<style>
+/* 样式 */
+  .app {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+</style>
