e3d5663f51768cad8fad2c4b2ae1bd9c7961b777
wangze <18802401646@163.com>
2024-01-11 10:29:55 +0800

    一个简单的效果 vue2语法

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,17 @@
 <template>
     <div class="app">
         <h1>你好</h1>
+        <Person/>
     </div>
 </template>
 
 
 <script lang="ts">
+  import Person from './components/Person.vue'
+
   export default {
-    name: 'App' //组件名
+    name: 'App', //组件名
+    components:{Person} //注册组件
   }
 </script>
 
